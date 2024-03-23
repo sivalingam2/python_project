4b74796db5bc7c31659af4e4d512fcfdb24e7445
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2024-03-23 14:18:37 +0530

    frontend update

--- frontend.py
+++ frontend.py
@@ -1,4 +1,7 @@
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
+frontend() {
+    yum install nginx -y
+    systemctl enable nginx
+    systemctl start nginx
+    rm -rf /usr/share/nginx/html/*
+}
+frontend()
